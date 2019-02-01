617ce952a4a7ed68d72780d9c6ba4a9b21a5d110
qapremise18 <47134072+qapremise18@users.noreply.github.com>
2019-02-01 20:53:25 +0530

    Add files via upload

--- maintest/test/TestMobile.py
+++ maintest/test/TestMobile.py
@@ -3,9 +3,9 @@ from maintest.TestBed import TestBed
 class TestMobile(TestBed):
 
   def test_sample(self):
-   print("HIIIIIIIi")
+   print("*********TESTCASE STARTED*********")
    self.driver.find_element_by_id("joinNowButton").click()
    self.driver.find_element_by_id("googleSignUpButton").click()
-   self.driver.find_element_by_id("googleSignUpButton").click()
+   self.driver.find_element_by_xpath("//*[@text='qapremise18@gmail.com']").click()
 if __name__ == '__main__':
     unittest.main()
