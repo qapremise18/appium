35235c9efc386a9ce8f9853d1359766ac798d0b2
qapremise18 <47134072+qapremise18@users.noreply.github.com>
2019-02-01 20:08:54 +0530

    Add files via upload

--- driver/CreateDriver.py
+++ driver/CreateDriver.py
@@ -4,25 +4,29 @@ import sys
 class CreateDriver:
 
  def createLocalDriver(self, capabilities, testBedConfig, testBedName) :
-        driver = None
+        self.driver = None
         self.capabilities = capabilities
         self.testBedConfig = testBedConfig
+        print(">>>>>self.capabilities",self.capabilities)
+        print(">>>>>self.testBedConfig", self.testBedConfig)
         print("*****Start createLocalDriver method*******")
         # serverUrl = "http://" + "127.0.0.1"+ ":" + testBedConfig.testBedName) +"/wd/hub")
-        serverUrl = "http://127.0.0.1:4723/wd/hub"
+        # serverUrl = "http://127.0.0.1:4723/wd/hub"
         try :
             print("*****Start createLocalDriver method*******")
-            self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", self.capabilities)
-            driver.implicitly_wait(20)
+            # self.driver = webdriver.Remote("http://127.0.0.1:4200/wd/hub", self.capabilities)
+            self.driver = webdriver.Remote("http://localhost:4723/wd/hub", self.capabilities)
+            self.driver.implicitly_wait(20)
             print("AppiumDriver successfully created:::createLocalDriver")
         except :
             print("Error while createLocalDriver:::"+sys.exc_info())
             print("Throwable while createLocalDriver:::")
+        return self.driver
 
  def getDriverTest(self):
      return self.driver
 
- def getDriver(capabilities, testBedConfig, testBedName):
+ def getDriver(self, capabilities, testBedConfig, testBedName):
       driver = None
-      CreateDriver.createLocalDriver(capabilities, testBedConfig, testBedName)
-      return driver;
+      return self.createLocalDriver(capabilities, testBedConfig, testBedName)
+      # return driver
