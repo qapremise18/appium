3154e8629305a246e1f4a728e50949f575d4dc73
qapremise18 <47134072+qapremise18@users.noreply.github.com>
2019-02-22 20:34:03 +0530

    Add files via upload

--- maintest/test/TestBed.py
+++ maintest/test/TestBed.py
@@ -6,18 +6,26 @@ import sys, time, unittest
 
 class TestBed(unittest.TestCase):
 
+    # def __init__(self):
+    #     self.testBedConfig = TestBedConfig()
+    #     self.testBedConfig.readConfig("D:\\PythonWS\\Premise\\resources\\config.properties")
+    #     # testBedConfig = TestBedConfig.test.getdriverinstance()
+    #     self.driverInstance = self.testBedConfig.getdriverinstance()
+    #     # self.testBedConfig = TestBedConfig()
+    #     # self.testBedConfig.getdriverinstance()
+
+
     def setUp(self):
       try :
             self.testBedConfig = TestBedConfig()
             self.testBedConfig.readConfig("D:\\PythonWS\\Premise\\resources\\config.properties")
-            # testBedConfig = TestBedConfig.test.getdriverinstance()
             self.driverInstance = self.testBedConfig.getdriverinstance()
             print("testBedConfig.getTestBedName>>>", self.testBedConfig.getTestBedName())
             print("testBedConfig.getTestBedName22>>>", self.testBedConfig.getPort())
             self.appLaunchObj = AppiumLauncher()
             self.appLaunchObj.closeAppiumSession(self.testBedConfig.getPort())
-            print("getPort>>>" + self.testBedConfig.getTestBedName())
-            print("TestBedName>>>"+ self.testBedConfig.getTestBedName())
+            print("getPort>>>" , self.testBedConfig.getTestBedName())
+            print("TestBedName>>>", self.testBedConfig.getTestBedName())
             self.driverInit(self.testBedConfig.getTestBedName())
             # loginTest = Login(testBedConfig.getDriver())
             # taskTest = Tasks(testBedConfig.getDriver())
@@ -25,11 +33,11 @@ class TestBed(unittest.TestCase):
             # paymentTest = Payment(testBedConfig.getDriver())
             # settingTest = Settings(testBedConfig.getDriver())
       except:
-            print("Unable to load the property file::: " + sys.exc_info())
+            print("Unable to load the property file::: " , sys.exc_info())
 
     def driverInit(self, testBedName) :
         # testBedConfig = TestBedConfig.getInstance()
-        print("Initializing Android driver for testBedName:::" + testBedName)
+        print("Initializing Android driver for testBedName:::" , testBedName)
         self.driver = None
         try :
             # app = AppiumLauncher()
@@ -42,37 +50,45 @@ class TestBed(unittest.TestCase):
             print("Driver created is :::", self.driver)
             self.testBedConfig.setDriver(self.driver)
         except :
-                print("Unable to initialize driver :::" + sys.exc_info())
+                print("Unable to initialize driver :::",  sys.exc_info())
 
 
 
     def launchApp(self):
-        testBedConfig = TestBedConfig.getInstance()
+        # testBedConfig = self.testBedConfig.getdriverinstance()
+        self.testBedConfig = TestBedConfig()
+        self.testBedConfig.readConfig("D:\\PythonWS\\Premise\\resources\\config.properties")
+        self.driverInstance = self.testBedConfig.getdriverinstance()
+
         try :
-            AppiumLauncher.executeCommand("adb -s " + testBedConfig.getUdid()
-                    + " shell am start -n " + testBedConfig.config.getValue("APPIUM_CONFIG", "APPPACKAGE") + "/"
-                    + testBedConfig.config.getValue("APPIUM_CONFIG", "APPACTIVITY"))
+            # AppiumLauncher.executeCommand("adb -s " + self.testBedConfig.getUdid()
+            #         + " shell am start -n " + self.testBedConfig.config.getValue("APPIUM_CONFIG", "APPPACKAGE") + "/"
+            #         + self.testBedConfig.config.getValue("APPIUM_CONFIG", "APPACTIVITY"))
+            AppiumLauncher.executeCommand("adb -s " + self.testBedConfig.getUdid()
+                                          + " shell am start -n " + self.testBedConfig.getappPackage()+ "/"
+                                          + self.testBedConfig.getappActivity())
             time.sleep(5)
             print("Relaunched App")
         except :
-            print("Unable to re launch App:::" + sys.exc_info())
+            print("Unable to re launch App:::" , sys.exc_info())
 
-    def closeAndRelaunchApp() :
+    def closeAndRelaunchApp(self) :
         print("In closeAndRelaunchApp()::::")
-        TestBed.closeApp()
-        TestBed.relaunchApp()
+        self.closeApp()
+        self.relaunchApp()
 
 
-    def closeApp() :
-        testBedConfig = TestBedConfig.getInstance()
+    def closeApp(self) :
+        testBedConfig = self.testBedConfig.getdriverinstance()
         AppiumLauncher.executeCommand("adb -s " + testBedConfig.getUdid()
                 + " shell am force-stop " + testBedConfig.appPackage)
         print("Closing the app")
 
 
-    def relaunchApp() :
-        testBedConfig = TestBedConfig.getInstance()
-        AppiumLauncher.executeCommand("adb -s " + testBedConfig.getUdid()+ " shell am start -n " + testBedConfig.appPackage + "/" + testBedConfig.appActivity)
+    def relaunchApp(self) :
+        testBedConfig = self.testBedConfig.getdriverinstance()
+        AppiumLauncher.executeCommand("adb -s " + testBedConfig.getUdid()+ " shell am start -n " +
+                                      testBedConfig.appPackage + "/" + testBedConfig.appActivity)
         print("Re launching app after closing app")
         time.sleep(3000)
         print("App Relaunched")
@@ -81,13 +97,24 @@ class TestBed(unittest.TestCase):
         self.clearAppData()
         self.launchApp()
         testBedConfig = self.testBedConfig.getdriverinstance()
-        testBedConfig.getDriver().resetApp()
+        # testBedConfig.getDriver().resetApp()
         print("Relaunched App")
 
     def clearAppData(self):
-        testBedConfig = self.testBedConfig.getdriverinstance()
-        AppiumLauncher.executeCommand("adb -s " + testBedConfig.getUdid()
-                                      + " shell pm clear " + testBedConfig.config.getValue("APPPACKAGE"))
+        self.testBedConfig = TestBedConfig()
+        self.testBedConfig.readConfig("D:\\PythonWS\\Premise\\resources\\config.properties")
+        self.driverInstance = self.testBedConfig.getdriverinstance()
+
+        # testBedConfig = self.testBedConfig.getdriverinstance()
+        print("testBedConfig#########",self.testBedConfig)
+        print("testBedConfig#########", self.testBedConfig==None)
+        print("testBedConfig#########", self.driverInstance == None)
+        print("testBedConfig#########", self.testBedConfig.getUdid())
+        # AppiumLauncher.executeCommand("adb -s " + self.testBedConfig.getUdid()
+        #                               + " shell pm clear " + self.testBedConfig.config.getValue("APPPACKAGE"))
+        AppiumLauncher.executeCommand("adb -s " + self.testBedConfig.getUdid()
+                                      + " shell pm clear " + self.testBedConfig.getappPackage())
+        time.sleep(3)
         time.sleep(3)
         print("Cleared App data")
     def tearDown(self):
--- maintest/test/TestMobile.py
+++ maintest/test/TestMobile.py
@@ -1,11 +1,23 @@
 import unittest
-from maintest.TestBed import TestBed
+from maintest.test.TestBed import TestBed
+from maintest.pages.Login import Login
 class TestMobile(TestBed):
 
-  def test_sample(self):
-   print("*********TESTCASE STARTED*********")
-   self.driver.find_element_by_id("joinNowButton").click()
-   self.driver.find_element_by_id("googleSignUpButton").click()
-   self.driver.find_element_by_xpath("//*[@text='qapremise18@gmail.com']").click()
+  # def test_sample(self):
+  #  print("*********TESTCASE STARTED*********")
+  #  self.driver.find_element_by_id("joinNowButton").click()
+  #  self.driver.find_element_by_id("googleSignUpButton").click()
+  #  self.driver.find_element_by_xpath("//*[@text='qapremise18@gmail.com']").click()
+  #  self.driver.find_element_by_class_name()
+  #  self.driver.find_elements_by_class_name()
+
+
+  def test_login(self):
+      print("test LOGIN>>>>>")
+      login = Login(self.driver)
+      login.verifyUserLogin("NewGmailPublicUser","US")
+
+
 if __name__ == '__main__':
     unittest.main()
+
